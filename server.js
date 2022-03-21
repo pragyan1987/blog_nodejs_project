c64ef37cf4b6c3099bf9895a77992b4073170f80
Pragyan <pragyan87.das@gmail.com>
2022-03-21 20:24:42 +0530

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const blogRouter2 = require('./routes/regi');
 
 const Blog = require('./models/Blog');
 const User= require('./models/data');
-const port=process.env.port ||5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -141,7 +141,7 @@ app.use('/regi', blogRouter2);
 
 //listen port
 //app.listen(5000);
-app.listen(port,() => {
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
 
-  console.log(port );
-})
